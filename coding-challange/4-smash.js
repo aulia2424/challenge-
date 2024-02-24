e81ffc7c764206de0c529761292fe984df49b9ba
aulia2424 <auliamartha243@gmail.com>
2024-02-24 18:13:25 +0700

    day 4 smash sentence

---
+++ coding-challange/4-smash.js
@@ -0,0 +1,12 @@
+// day 4
+// with join
+// function smash(words) {
+//     return words.join(' ')
+// };
+// console.log(smash(['this', 'is', 'a', 'really', 'long', 'sentence']))
+
+// day 4
+// with arrow function
+// simpel & praktis 
+const smash = (words) => words.join(' ')
+console.log(smash(['this', 'is', 'a', 'really', 'long', 'sentence'])) 
