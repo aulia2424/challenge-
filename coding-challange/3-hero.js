31bdd553a60a34259565e02acdff34001bb355bb
aulia2424 <auliamartha243@gmail.com>
2024-02-24 17:25:01 +0700

    day 3 hero

---
+++ coding-challange/3-hero.js
@@ -0,0 +1,19 @@
+// if else
+// function hero(bullets, dragons){
+//     if(bullets / 2 >= dragons) {
+//         return true
+//     } else {
+//         return false
+//     }
+// }
+// console.log(hero(10, 5))
+
+// ternary
+// function hero(bullets, dragons){
+//     return bullets / 2 >= dragons ? true : false;
+// }
+// console.log(hero(10, 5))
+
+// CARA TIGA
+const hero = (bullets, dragons) => bullets /2 >= dragons;
+console.log(hero(10, 5))
